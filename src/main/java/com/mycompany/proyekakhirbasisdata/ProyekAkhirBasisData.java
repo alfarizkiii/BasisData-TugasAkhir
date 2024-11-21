4241fec0498506e64661e4b24ac7d2e12e0add2c
PPiR0604 <mfahraviisk@gmail.con>
2024-11-21 20:51:00 +0700

    Init

---
+++ src/main/java/com/mycompany/proyekakhirbasisdata/ProyekAkhirBasisData.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyekakhirbasisdata;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ProyekAkhirBasisData {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
